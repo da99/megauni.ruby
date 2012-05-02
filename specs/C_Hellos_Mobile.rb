ce04c134f2f1342a88848ef33f56fa2348cde994
da99 <i-hate-spam-45671204@mailinator.com>
2012-05-02 04:19:13 -0500

    Updated: specs for mobile urls.

--- specs/C_Hellos_Mobile.rb
+++ specs/C_Hellos_Mobile.rb
@@ -1,33 +1,33 @@
 # controls/Hellos.rb
 
 
-class Test_Control_Hellos_Mobile < Test::Unit::TestCase
+describe :Control_Hellos_Mobile do
 
-  must 'sets the mobilize cookie and redirects /m/ to homepage' do
+  it 'sets the mobilize cookie and redirects /m/ to homepage' do
     get '/m/'
     follow_redirect!
     assert_equal '/', last_request.fullpath
     assert_equal 'yes', last_request.cookies['use_mobile_version']
   end
 
-  must 'add a slash to the mobile homepage path: /m' do
+  it 'add a slash to the mobile homepage path: /m' do
     get '/m'
     follow_redirect!
     follow_redirect!
     assert_equal '/', last_request.fullpath
   end
 
-  must 'redirects /salud/m/ to /salud/' do
+  it 'redirects /salud/m/ to /salud/' do
     get '/salud/m/'
     assert_redirect '/salud/', 303
   end
 
-  must 'redirects /help/m/ to /help/' do
+  it 'redirects /help/m/ to /help/' do
     get '/help/m/' 
     assert_redirect '/help/', 303
   end
 
-  must 'redirects the following to /salud/m/: /saludm/ /saludm/ /saludmobi/ /saludiphone/ /saludpda/' do
+  it 'redirects the following to /salud/m/: /saludm/ /saludm/ /saludmobi/ /saludiphone/ /saludpda/' do
     %w{ /saludm/ /saludm/ /saludmobi/ /saludiphone/ /saludpda/ }.each { |url|
       get url
       follow_redirect!
