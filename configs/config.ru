7c924b545e568b8666b4a0a5c097d71ba2a194c4
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-24 23:54:25 -0500

    Refactoring.

--- configs/config.ru
+++ configs/config.ru
@@ -1,49 +1,9 @@
 
-ENV['RACK_ENV'] ||= 'production'
-
-require 'rack/protection'
-require 'securerandom'
-require './Server/main'
+require './megauni'
 
 
 # === Protective
 
-class Da_Middles
-end
-
-
-use Da_Middles
-use Rack::Lint
 run Cuba
-
-
-# %w{
-
-  # Surfer_Hearts_Archive
-
-  # Redirect_Mobile
-
-  # Mu_Archive_Redirect
-
-  # Mu_Archive
-
-# }.each { |name|
-  # require "./middleware/#{name}"
-  # use Object.const_get(name)
-# }
-
-# require './middleware/Public_Files'
-# use Public_Files, ['public/busy-noise', 'public/my-egg-timer', 'public/styles', 'public/skins', Mu_Archive::Dir, Surfer_Hearts_Archive::Dir]
-
-# # === Content Generators
-
-# # === Helpers
-# use Rack::MethodOverride
-
-# # === Low-level Helpers
-# # === (specifically designed to run before Uni_App).
-
-# # Finally, start the app.
-# run Cuba
 
 
--- megauni.rb
+++ megauni.rb
@@ -1,42 +1,107 @@
+# === The most basic stuff first... ===
+
+$KCODE = 'UTF8'
+require 'jcode'
+
+ENV['RACK_ENV'] ||= 'development'
+require "always_verify_ssl_certificates"
 
 require 'sinatra/base'
-require "Dex"
+require 'mongo'
+require 'mongo_rack'
+require 'helpers/mongo_rack_with_proper_uri_parser'
 
-Dex.db "/apps/mu.dex.db"
-
-class The_App < Sinatra::Base
-  HTTP_Status_Error = Class.new(RuntimeError)
-  Perma = 301
-  Missing = 404
-  set :sessions, false
-
-  get "/today/" do
-    redirect to("/"), Perma
-  end
-
-  %w{ sitemap rss }.each { |name|
-    get "/#{name}.xml" do
-      headers 'Content-Type' => 'application/xml;charset=utf-8'
-      Mu_Archive_Read "/#{name}.xml"
-    end
+  %w{
+    Allow_Only_Roman_Uri
+    Squeeze_Uri_Dots 
+    Find_The_Bunny
+    Always_Find_Favicon
+    Slashify_Path_Ending
+    Serve_Public_Folder
+    Redirect_Mobile
+    Catch_Bad_Bunny
+    Email_Exception
+    Strip_If_Head_Request
+    Flash_Msg
+    Mu_Archive_Redirect
+  }.each { |middle|
+    require "middleware/#{middle}"
   }
 
-  get '/sports/' do
-    %~
-     <html>
-     <body>
-     Not done. In the meantime, check out
-     <a href="http://blog.mises.org/5402/the-amtraking-of-sports/">this article</a>.
-     </body>
-     </html>
-    ~
-  end
+require 'helpers/app/kernel' 
+
+# === Require configurations. ===
+class Uni_App < Sinatra::Base
+end # ===
+require 'configs/Uni_App'
+require 'configs/DB'
+
+# === Require models. ===
+
+%w{
+  Doc_Log
+  Club
+  Message
+  Member
+}.each { |mod| require "models/#{mod}" }
+
+
+# === Require controls. ===
+
+%w{
+  Base_Control
+  Hellos
+  Clubs
+  Sessions
+  Members
+  Messages
+}.each { |control|
+  require "controls/#{control}"
+}
+
+# if Uni_App.development?
+#   require "controls/Inspect_Control"
+#   # Uni_App.controls << Inspect_Control
+# end
+  
+# -------------- NEWER CODE -----------------------------
+%w{
+
+   Surfer_Hearts_Archive
+
+   Redirect_Mobile
+
+   Mu_Archive_Redirect
+
+   Mu_Archive
+
+}.each { |name|
+ require "./middleware/#{name}"
+ use Object.const_get(name)
+}
+
+require './middleware/Public_Files'
+use Public_Files, ['public/busy-noise', 'public/my-egg-timer', 'public/styles', 'public/skins', Mu_Archive::Dir, Surfer_Hearts_Archive::Dir]
+
+# === Content Generators
+
+# === Helpers
+use Rack::MethodOverride
+
+# === Low-level Helpers
+# === (specifically designed to run before Uni_App).
+
+# Finally, start the app.
+run Cuba
+
+
+
 
   post "/search/" do 
     name = params['name'] || params['keyword']
     retro = "/#{name}/"
     redirect(to("/#{name}/"), Perma) if Mu_Archive(retro)
-    
+
     case name
     when %r!menop..se!, %r!ost[eo]+p[oris]+!
       redirect to("/meno-osteo/"), Perma
@@ -49,4 +114,5 @@ class The_App < Sinatra::Base
     redirect to("/search/"), Perma
   end
 
-end # === class The_App
+
+
