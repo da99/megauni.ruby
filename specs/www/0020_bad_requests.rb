7b2c928b6edf16b4dd2fc0dc951d67dedafe0210
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-27 23:13:07 -0500

    Typos. Removed: obsolete tests

--- specs/www/0020_bad_requests.rb
+++ specs/www/0020_bad_requests.rb
@@ -5,29 +5,18 @@ describe :Control_Bad_Requests do
 
   it 'redirect any favicon.ico requests to /favicon.ico' do
     get "/my-egg-timer/favicon.ico"
-    follow_redirect!
-    "/favicon.ico".should == last_request.fullpath
+    redirect_url.should == "/favicon.ico"
   end
-  
+
   it 'redirect /SWF/main.swf to http://www.bing.com/SWF/main.swf' do
     get "/SWF/main.swf"
-    follow_redirect!
-    "http://www.bing.com/SWF/main.swf".should == last_response.headers['Location']
+    redirect_url.should == "http://www.bing.com/SWF/main.swf"
   end
 
   it 'redirect /(null)/ to http://www.bing.com/(null)/' do
     get "/(null)/"
-    follow_redirect!
-    "http://www.bing.com/(null)/".should == last_response.headers['Location']
+    http_code.should == 400
   end
-
-  %w{ vb forum forums old vbulletin}.each { |dir|
-    it "redirects /#{dir}/ to #{BING_URL} if Googlebot" do
-      header 'USER_AGENT', 'SOMETHING Googlebot/5.1'
-      get "/#{dir}/"
-      redirects_to BING_URL
-    end
-  }
 
   it "redirects w/ 301: /manager/status/ -> http://www.honoringhomer.net/" do
     get "/manager/status/"
@@ -41,8 +30,7 @@ describe :Control_Bad_Requests do
 
   it "redirects w/#{PERM} /admin/spaw/spacer.gif -> #{BING_URL}" do
     get "/admin/spaw/spacer.gif"
-    http_code.should == PERM
-    redirect_url.should == BING_URL
+    redirects_to PERM, BING_URL
   end
 
 end # === class Test_Control_Bad_Requests
