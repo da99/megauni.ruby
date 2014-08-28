bfda119f5624e4dcfc90290d5239ac792f1cf5de
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-28 03:10:04 -0500

    Added: tests for /my-egg-timer public files.

--- specs/www/0050_public_files.rb
+++ specs/www/0050_public_files.rb
@@ -1,7 +1,7 @@
 
 describe "public files" do
 
-  it "retrieves swf with content-type: application/x-shockwave-flash" do
+  it "retrieves /busy-noise swf with content-type: application/x-shockwave-flash" do
     get(
       "/busy-noise/media/button_player/button/musicplayer_f6.swf" +
       "?&autoplay=true&repeat=true&song_url=http://megauni.s3.amazonaws.com/beeping.mp3"
@@ -9,8 +9,13 @@ describe "public files" do
     content_type.should == "application/x-shockwave-flash"
   end
 
-  it "retrieves js with content-type: application/javascript" do
+  it "retrieves /busy-noise js with content-type: application/javascript" do
     get("/busy-noise/javascripts/chicken.js?v122f83805")
+    content_type.should == "application/javascript"
+  end
+
+  it "retrieves /my-egg-timer js with content-type: application/javascript" do
+    get "http://localhost:4567/my-egg-timer/javascripts/mootools.js?v=07.04.2009.07.15.04"
     content_type.should == "application/javascript"
   end
 
