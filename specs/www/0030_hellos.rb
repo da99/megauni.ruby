6a2d0f7c102bc84af435fee6cdcec4d7a252f142
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-27 23:21:51 -0500

    Dev. checkpoint.

--- specs/www/0030_hellos.rb
+++ specs/www/0030_hellos.rb
@@ -1,54 +1,32 @@
 # controls/Hellos.rb
 
 
-describe :Control_Hellos do
+describe '/' do
 
   it 'show homepage: /' do
     get '/'
     assert_equal 200, last_response.status
   end
 
-  it 'respond to HEAD /' do
-    head '/'
-    assert_equal 200, last_response.status
-  end
-
-  it 'respond to HEAD /salud/' do
-    head '/salud/'
-    assert_equal 200, last_response.status
+  it 'respond to HEAD /salud' do
+    head '/salud'
+    http_code.should == 200
   end
 
   it "shows /busy-noise" do
     get '/busy-noise'
-    follow_redirect!
-    assert_equal 200, last_response.status
+    http_code.should == 200
   end
 
   it "shows /my-egg-timer" do
     get '/my-egg-timer'
-    follow_redirect!
-    assert_equal 200, last_response.status
-  end
-
-  it "adds a slash to a path" do
-    get '/busy-noise'
-    last_response.headers['Location'].should.match /noise\/$/
-  end
-
-  it "adds a slash to path, not query string" do
-    get '/shiina-ringo?album=1'
-    should_redirect "/shiina-ringo/?album=1", 301
-  end
-
-  it "does not add a slash to a missing file: cat.png" do
-    get '/dir/cat.png'
-    assert_equal 404, last_response.status
+    http_code.should == 200
   end
 
   it "renders /sitemap.xml as xml" do
     get '/sitemap.xml' 
-    assert_equal 200, last_response.status
-    assert_equal 'application/xml;charset=utf-8', last_response.content_type
+    http_code.should == 200
+    content_type.should == 'application/xml;charset=utf-8'
   end
 
   it "renders /help/" do
